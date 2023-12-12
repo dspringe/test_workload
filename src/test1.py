974b6ea145fde02f0ef6ca40d537082515b29522
dspringe <daniel.springer@intel.com>
2023-12-12 10:57:11 -0800

    Update output file label to support args

--- src/test1.py
+++ src/test1.py
@@ -4,6 +4,7 @@ import platform
 import psutil
 import random
 import socket
+import sys
 import time
 
 def print_os_info():
@@ -14,7 +15,7 @@ def print_os_info():
     print(f"Processor: {platform.processor()}")
     print()
 
-def print_system_stats():
+def print_system_stats(args):
     print("System Stats:")
     cpu_usage = psutil.cpu_percent()
     memory_usage = psutil.virtual_memory().percent
@@ -23,14 +24,15 @@ def print_system_stats():
     print(f"Memory Usage: {memory_usage}%")
     print()
 
-    dump_output_files(cpu_usage, memory_usage)
+    dump_output_files(cpu_usage, memory_usage, args)
 
-def dump_output_files(cpu_usage, memory_usage):
+def dump_output_files(cpu_usage, memory_usage, args):
     print("Dumping output files with system stats...")
-    hostname = socket.gethostname()
+    
+    label = socket.gethostname() if not args else args[1]
     ts = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d-%H-%M-%S')
-    cpu_log = f"{hostname}_cpu_usage_{ts}.log"
-    memory_log = f"{hostname}_memory_usage_{ts}.log"
+    cpu_log = f"{label}_cpu_usage_{ts}.log"
+    memory_log = f"{label}_memory_usage_{ts}.log"
     
     os.system(f"echo {cpu_usage} > {cpu_log}")
     os.system(f"echo {memory_usage} > {memory_log}")
@@ -39,16 +41,16 @@ def dump_output_files(cpu_usage, memory_usage):
     print(f"Collect Memory Usage log: {os.path.abspath(memory_log)}")
     print()
 
-def main(sleep_time):
+def main(sleep_time, args):
     print("Hello, world!")
 
     print_os_info()
     print(f"sleeping for {sleep_time}")
     time.sleep(sleep_time)
 
-    print_system_stats()
+    print_system_stats(args)
 
 if __name__ == "__main__":
     sleep_time = random.randint(4, 20)  
-    main(sleep_time)
+    main(sleep_time, sys.argv[1:])
 
